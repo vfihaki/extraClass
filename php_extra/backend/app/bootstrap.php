94fe1453833cd3f67d127ca6bef739885496d625
Viliami Fihaki <30000357@student.toiohomai.ac.nz>
2019-10-11 16:00:21 +1300

    commit

---
+++ php_extra/backend/app/bootstrap.php
@@ -0,0 +1,8 @@
+<?php
+
+    require_once('helper/startup.php');
+    require_once('libraries/Core.php');
+    require_once('libraries/Controller.php');
+    require_once('libraries/Database.php');
+
+?>
